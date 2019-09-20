a1a7b83ccd3680f66be7a1e7f022d687192f5530
quxiaoming <quxiaoming@szzt.com.cn>
2019-09-20 17:42:24 +0800

    【增加】热更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,21 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'vue.js',
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: false,
+        hot: true,
+        port: 9000,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: 'index.html', //通过模板生成的文件名
+            template: 'index.html', //模板路径
+            inject: false, //是否自动在模板文件添加 自动生成的js文件链接
+            title: '这个是WebPack Demo',
+            minify: {
+                removeComments: true, //是否压缩时 去除注释
+            },
+        }),
+    ],
 };
